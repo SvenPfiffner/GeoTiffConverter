a7771e911dc1e9741a13e9ca0302028aef6556fa
SvenPfiffner <sven.pfiffner@gmail.com>
2023-11-06 21:31:08 +0100

    Prepared UI

--- gui.py
+++ gui.py
@@ -12,17 +12,23 @@ def generate_mesh(tiff_raw, downsample, add_base, height):
     tiff.MeshUtil.point_cloud_to_mesh(xyz, "data/mesh.obj", downsample_voxel_size=downsample, add_base=base_flag)
     return "data/mesh.obj"
 
+def retrieve_geo_data(tiff_raw):
+    pass
+
+def retrieve_meta_data(tiff_raw):
+    pass
+
 with gr.Blocks() as demo:
 
-    # Single Image Tab
-    with gr.Tab("Elevation-TIFF"):
+    # Elevation tab
+    with gr.Tab("Elevation"):
         # Input Tiff
-        input_tiffs = gr.File(label="Input TIFFs", file_count='multiple', file_types=[".tif"])
+        input_tiffs_elevation = gr.File(label="Input TIFFs", file_count='multiple', file_types=[".tif"])
         # IO
         with gr.Row():
             #Visualization site
             with gr.Column():
-                visualization_button = gr.Button("Visualize Height Information")
+                visualization_button = gr.Button("Visualize Elevation Information")
                 visualization_output = gr.Plot()
             #Mesh site
             with gr.Column():
@@ -33,8 +39,34 @@ with gr.Blocks() as demo:
                 mesh_button = gr.Button("Generate Mesh")
                 mesh_output = gr.Model3D()
 
+    # Data analysis tab
+    with gr.Tab("Data Analysis"):
+        # Input Tiff
+        input_tiff_analysis = gr.File(label="Input TIFFs", file_count='single', file_types=[".tif"])
+        # IO
+        with gr.Row():
+            #Plotting site
+            with gr.Column():
+                data_analysis_plots_button = gr.Button("Plot characteristics")
+                with gr.Row():
+                    gr.Plot()
+                    gr.Plot()
+                with gr.Row():
+                    gr.Plot()
+                    gr.Plot()
+            with gr.Column():
+                data_analysis_geo_button = gr.Button("Retrieve Geo-Information")
+                gr.Plot()
+                gr.Plot()
+            with gr.Column():
+                data_analysis_meta_button = gr.Button("Show Metadata")
+                gr.Textbox(lines=10)
 
-    visualization_button.click(fn=visualize_tif, inputs=[input_tiffs], outputs=visualization_output)
-    mesh_button.click(fn=generate_mesh, inputs=[input_tiffs, voxel_downsampling, include_base, lowest_height], outputs=mesh_output)
+
+
+    visualization_button.click(fn=visualize_tif, inputs=[input_tiffs_elevation], outputs=visualization_output)
+    mesh_button.click(fn=generate_mesh, inputs=[input_tiffs_elevation, voxel_downsampling, include_base, lowest_height], outputs=mesh_output)
+    data_analysis_geo_button.click(fn=retrieve_geo_data, inputs=[input_tiff_analysis])
+    data_analysis_meta_button.click(fn=retrieve_meta_data, inputs=[input_tiff_analysis])
 
 demo.launch()
