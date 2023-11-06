34754f873456a36d9cb5ca155c812e878ec7d44b
SvenPfiffner <sven.pfiffner@gmail.com>
2023-11-06 15:28:00 +0100

    Updated .gitignore

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import GeoTIFFConverter as tiff
-import numpy as np
-
-x, o = np.linspace(1, 16, 4).reshape((-1, 4)), np.ones((4, 1))
-X = np.matmul(o, x)
-Y = X.copy().transpose()
-coord = np.dstack([X,Y]).reshape((-1, 2))
-
-print(X)
-print(Y)
-print(coord)
-#tiff.MeshUtil.test()
-
-print(tiff.Converter.to_point_cloud(file1)[0])
+# Sandbox
